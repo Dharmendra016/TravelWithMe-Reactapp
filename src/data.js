86e55f35b190fe41a5370ed160b3a6ae5526562b
Dharmendra Singh Chaudhary <dharmendra.078bct016@tcioe.edu.np>
2023-07-18 19:30:48 +0545

    commiting all files

---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = [
+
+    {
+        id:1, 
+        
+        name:"Agra" , 
+        
+        info:"The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor, Shah Jahan (reigned from 1628 to 1658), to house the tomb of his favourite wife, Mumtaz Mahal.",
+
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Taj_Mahal%2C_Agra%2C_India_edit3.jpg/1024px-Taj_Mahal%2C_Agra%2C_India_edit3.jpg",
+        
+        price:"35,785",
+    },
+
+    {
+        id:2, 
+        name:"Pokhara" , 
+        info:"Pokhara (Nepali: पोखरा [ˈpokʰʌɾa]) is a metropolitan city in central Nepal, which serves as the capital of Gandaki Province.[4] It is the second most populous city of Nepal after Kathmandu, with 599,504 inhabitants living in 120,594 households in 2021.[3] It is the country's largest metropolitan city in terms of area. The city also serves as the headquarters of Kaski District.",
+        image:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/d6/96/36/photo4jpg.jpg?w=1200&h=-1&s=1",
+        price:"35,785",
+    },
+
+    {
+        id:3, 
+        name:"Kathmandu" , 
+        info:"Momos. Maybe I was being culturally obtuse, or simply greedy, when my first thought about Kathmandu led to a mental image of the delectable, steaming dumpling, followed by another of misty mountains, of course. The capital city of Nepal was the first stop on my short tour of the country.",
+        image:"https://assets.vogue.in/photos/5ce431b346cf5953f8b18c74/2:3/w_1600,c_limit/featured.2.jpg",
+
+        price:"40,452",
+    },
+
+    {
+        id:4, 
+
+        name:"Surkhet" , 
+
+        info:"Birendranagar (Nepali: वीरेन्द्रनगर) officially Birendranagar Municipality is a city in Surkhet District in Karnali Province of Nepal.[2] It is the district headquarter of Surkhet District and also the capital city of Karnali province. As of March 2022, Birendranagar has a population of 154,886 making it the 17th largest city of Nepal. It is the largest city of karnali province and 7th largest in Western Nepal. It is one of the fastest growing cities of Nepal.",
+
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Ghantaghar%40surkhet.JPG/250px-Ghantaghar%40surkhet.JPG",
+        price:"20,000",
+    },
+
+    {
+        id:5, 
+        name:"Kalinchok" , 
+        info:"Kalinchowk is a hill station and a tourist hotspot. It is located at 3842 meters of altitude and about 150km northeast from national capital Kathmandu. The place is best known for trekking and skiing.[5] During the December, January and February (mainly Paush and Magh in Nepali months) snowfalls in Kalinchowk",
+
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Kuri_Village.jpg/800px-Kuri_Village.jpg",
+        price:"35,000",
+    }
+
+];
+
+
+export default data ; 
